49c4a8f37a004f5d5049eca970a78be91e41d99d
Thiago Franchin <thiago.franchin@i-value.int>
2016-05-20 17:35:07 -0300

    Atualização dos buttons (text-uppercase)

--- js/functions.js
+++ js/functions.js
@@ -42,7 +42,10 @@ $(document).ready(function(){
         'color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15 color16 color17 color18 color19 color20'
       ).addClass('color6');
     }
-  });  
+  });
+
+  $('button').addClass('text-uppercase');
+
 
 
   //** FORMS  **//
