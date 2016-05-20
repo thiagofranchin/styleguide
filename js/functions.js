9244b749449b4c86d7149d1c56e0b2b921cabe37
Thiago Franchin <thiago.franchin@i-value.int>
2016-05-20 16:51:12 -0300

    Add script alteração de cor de classes externas ao bootstrap

--- js/functions.js
+++ js/functions.js
@@ -24,11 +24,29 @@ $(document).ready(function(){
   $(".menu-marcas > div").click(function(){
     newClass = $(this).attr("newclass");
     $("head #linkcss").attr("href","css/"+newClass+"/bootstrap.min.css");
+
+    if(newClass == "saas"){
+      $(".drop-box i, .drop-box em").removeClass(
+        'color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15 color16 color17 color18 color19 color20'
+      ).addClass('color2');
+    }
+
+    if(newClass == "mkt-digital"){
+      $(".drop-box i, .drop-box em").removeClass(
+        'color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15 color16 color17 color18 color19 color20'
+      ).addClass('color3');
+    }
+
+    if(newClass == "remanescentes"){
+      $(".drop-box i, .drop-box em").removeClass(
+        'color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15 color16 color17 color18 color19 color20'
+      ).addClass('color6');
+    }
   });  
 
 
   //** FORMS  **//
-  
+
 
 
   /**********************/
