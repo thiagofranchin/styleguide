cfe554614a20dc185cc9ba86078703954a64d34d
Thiago Franchin <thiago.franchin@i-value.int>
2016-05-23 18:09:28 -0300

    Início dos Cards com Bootcards.org

--- js/functions.js
+++ js/functions.js
@@ -25,28 +25,60 @@ $(document).ready(function(){
     newClass = $(this).attr("newclass");
     $("head #linkcss").attr("href","css/"+newClass+"/bootstrap.min.css");
 
+
+
+
     if(newClass == "saas"){
-      $(".drop-box i, .drop-box em").removeClass(
+      $(".drop-box i, .drop-box div").removeClass(
         'color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15 color16 color17 color18 color19 color20'
       ).addClass('color2');
+
+      $("#cards .card-count").removeClass(
+        'bgColor1 bgColor2 bgColor3 bgColor4 bgColor5 bgColor6 bgColor7 bgColor8 bgColor9 bgColor10 bgColor11 bgColor12 bgColor13 bgColor14 bgColor15 bgColor16 bgColor17 bgColor18 bgColor19 bgColor20'
+      ).addClass('bgColor2');
     }
+
+
+
 
     if(newClass == "mkt-digital"){
-      $(".drop-box i, .drop-box em").removeClass(
+      $(".drop-box i, .drop-box div").removeClass(
         'color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15 color16 color17 color18 color19 color20'
       ).addClass('color3');
+
+      $("#cards .card-count").removeClass(
+        'bgColor1 bgColor2 bgColor3 bgColor4 bgColor5 bgColor6 bgColor7 bgColor8 bgColor9 bgColor10 bgColor11 bgColor12 bgColor13 bgColor14 bgColor15 bgColor16 bgColor17 bgColor18 bgColor19 bgColor20'
+      ).addClass('bgColor3');
     }
+
+
+
 
     if(newClass == "remanescentes"){
-      $(".drop-box i, .drop-box em").removeClass(
+      $(".drop-box i, .drop-box div").removeClass(
         'color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15 color16 color17 color18 color19 color20'
       ).addClass('color6');
+
+      $("#cards .card-count").removeClass(
+        'bgColor1 bgColor2 bgColor3 bgColor4 bgColor5 bgColor6 bgColor7 bgColor8 bgColor9 bgColor10 bgColor11 bgColor12 bgColor13 bgColor14 bgColor15 bgColor16 bgColor17 bgColor18 bgColor19 bgColor20'
+      ).addClass('bgColor6');
     }
+
+
+
   });
 
-  $('button').addClass('text-uppercase');
 
+  $('button, a').addClass('text-uppercase');
 
+//** BOOTSTRAP CARDS **//
+  bootcards.init( {
+    offCanvasBackdrop : true,
+    offCanvasHideOnMainClick : true,
+    enableTabletPortraitMode : true,
+    disableRubberBanding : true,
+    disableBreakoutSelector : 'a.no-break-out'
+    });
 
   //** FORMS  **//
 
